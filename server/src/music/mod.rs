901535418ae70e55d393b05e8bd85337c4bfc612
Pixsky8 <pixsky8@gmail.com>
2021-02-28 23:00:20 +0100

    Server: added config for volume control

--- server/src/music/mod.rs
+++ server/src/music/mod.rs
@@ -3,9 +3,32 @@ use std::io::BufReader;
 
 pub mod mp3;
 
+pub struct Config {
+    min_volume: u8,
+    max_volume: u8,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        Config {
+            min_volume: 0,
+            max_volume: 150,
+        }
+    }
+
+    pub fn set_max_volume(&mut self, new: u8) {
+        self.max_volume = new;
+    }
+
+    pub fn set_min_volume(&mut self, new: u8) {
+        self.min_volume = new;
+    }
+}
+
 pub struct Music {
     sink: rodio::Sink,
     path_queue: Vec<mp3::Mp3>,
+    config: Config,
 
     #[allow(dead_code)]
     stream: rodio::OutputStream,
@@ -23,10 +46,17 @@ impl Music {
             stream: stream_,
             sink: rodio::Sink::try_new(&stream_handle_).unwrap(),
             path_queue: Vec::new(),
+            config: Config::new(),
         }
     }
 
     pub fn change_volume(&mut self, volume: u8) {
+        let volume = match volume {
+            v if v > self.config.max_volume => self.config.max_volume,
+            v if v < self.config.min_volume => self.config.min_volume,
+            v => v,
+        };
+
         let real_value: f32 = (volume as f32) / 100.0;
 
         self.sink.set_volume(real_value);
