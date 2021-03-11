1f1fe69c8fe275e6786fad78fc239888d98516a5
Pixsky8 <pixsky8@gmail.com>
2021-03-11 21:34:09 +0100

    web client: better navigation tab bar

--- web-client/src/app/app.component.ts
+++ web-client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, Routes } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'web-client';
+    title = 'Music Player';
+    navTabs = [
+        { path: '/', label: 'Queue' },
+        { path: '/request', label: 'Request' },
+    ];
 }
